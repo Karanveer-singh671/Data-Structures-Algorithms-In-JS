90e9d2e5f16662a4985df7d02580f2c8ceca021c
Karanveer Singh <singhkaranveer671@gmail.com>
2020-11-09 10:52:54 -0800

    firstRecurringCharacter

--- HashTables/firstRecurringCharacter.js
+++ HashTables/firstRecurringCharacter.js
@@ -7,3 +7,23 @@
 //Given an array = [2,3,4,5]:
 //It should return undefined
 
+function firstRecurringCharacter3(array1) {
+	// check less than 1 or equal to return undefined
+	if (array1.length <= 1) {
+		return undefined;
+	}
+	let obj = {};
+	for (let i = 0; i < array1.length; i++) {
+		// if obj has the value return the array value
+		if (obj[array1[i]]) {
+			return array1[i];
+		} else {
+			// add to value to object as key if doesn't exist
+			obj[array1[i]] = 1;
+		}
+	}
+	return undefined;
+}
+
+console.log(firstRecurringCharacter3([1, 5, 5, 1, 3, 4, 6]));
+console.log(firstRecurringCharacter3([2, 5, 5, 2, 3, 5, 1, 2, 4]));
