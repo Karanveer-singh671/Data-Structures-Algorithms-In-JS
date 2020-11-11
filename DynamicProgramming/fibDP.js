b9377a2e5f1b8885a71c9320b6d7ab856ac1a70c
Karanveer Singh <singhkaranveer671@gmail.com>
2020-11-11 09:41:58 -0800

    bottom up

--- DynamicProgramming/fibDP.js
+++ DynamicProgramming/fibDP.js
@@ -1,20 +1,29 @@
 function fibDP() {
-  // create a closure 
-  // cache the first and second values in cache
+	// create a closure
+	// cache the first and second values in cache
 	let cache = {};
 	cache[0] = 0;
 	cache[1] = 1;
 	return function fib(n) {
-    // inside closure check if value exists in cache
+		// inside closure check if value exists in cache
 		if (n in cache) {
 			return cache[n];
 		} else {
-      // store computed value in cache 
-      cache[n] = fib(n - 1) + fib(n - 2);
-      // return to go to next call in call stack
+			// store computed value in cache
+			cache[n] = fib(n - 1) + fib(n - 2);
+			// return to go to next call in call stack
 			return cache[n];
 		}
 	};
 }
-const fib = fibDP()
-console.log(fib(10))
+const fib = fibDP();
+console.log(fib(10));
+
+
+function fibBottomUp(n) {
+	let answer = [0, 1];
+	for (let i = 0; i < n; i++) {
+		answer.push(answer[i - 2] + answer[i - 1]);
+	}
+	return answer.pop();
+}
