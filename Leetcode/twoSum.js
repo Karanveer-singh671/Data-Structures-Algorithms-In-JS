2b1c133609b0b7b2965903c71f2f5595eafd5148
Karanveer Singh <singhkaranveer671@gmail.com>
2020-11-13 13:47:50 -0800

    towSum optimal hashmap solution

--- Leetcode/twoSum.js
+++ Leetcode/twoSum.js
@@ -7,16 +7,23 @@
 //* 5. Can there be multiple pairs that add up to the target?
 //* 6. Is the array sorted?
 //* Write the steps the algorithm will take to achieve its goal
+//* Write or discuss a brute force algorithm
+//* talk about drawbacks and optimization
+//* code out optimization
+//* check for syntax errors or any other glaring errors
+//* run through a test case to verify solution
 
 function bruteForceTwoSum(nums, target) {
+	// T: O(N^2), S: O(1) (is there a way to make Space higher and reduce time complexity, make one worse to make one better)
 	if (nums.length <= 1) {
 		return null;
 	}
 	let numberToFind;
 	for (let i = 0; i < nums.length - 1; i++) {
-    numberToFind = target - nums[i];
-    // need i + 1 instead of 1 since when finish inner loop only outer loop is incremented and reinitialize the loop!
-    // best practice to do j = i + 1
+		console.log({ i, value: nums[i] });
+		numberToFind = target - nums[i];
+		// need i + 1 instead of 1 since when finish inner loop only outer loop is incremented and reinitialize the loop!
+		// best practice to do j = i + 1
 		for (let j = i + 1; j < nums.length; j++) {
 			if (nums[j] === numberToFind) {
 				return [i, j];
@@ -25,3 +32,53 @@ function bruteForceTwoSum(nums, target) {
 	}
 	return null;
 }
+// use a hashmap lookup is O(1) assuming good hash function
+function twoSumOptimal(nums, target) { // T: O(n), S: O(n)
+	if (nums.length <= 1) {
+		return null;
+	}
+	let numberToFind;
+	// Optimize by breaking two for loops into non nested
+	let numsMap = {};
+	// go through each element in array and store the numberToFind as a key and index as a value
+	for (let i = 0; i < nums.length; i++) {
+		// could be number or undefined
+		const numsMapVal = numsMap[nums[i]];
+		if (numsMapVal >= 0) {
+			return [numsMapVal, i];
+		} else {
+			numberToFind = target - nums[i];
+			numsMap[numberToFind] = i;
+		}
+	}
+	return null;
+}
+
+
+function twoSum(nums, target) {
+  if(nums.length <=1) {
+    return null
+  }
+  let numsMap = {}
+  let numberToFind;
+  let numsMapValue;
+  for(let i = 0; i < nums.length;i++) {
+    numsMapValue = numsMap[nums[i]]
+    if (numsMapValue == null) {
+      numberToFind = target - nums[i]
+      numsMap[numberToFind] = i
+    } else {
+      return [numsMapValue, i]
+    }
+  }
+  return null
+}
+
+const nums = [1, 3, 7, 9, 2]; // target = 11
+
+/* obj = {
+  10:0,
+  8:1,
+  4:2,
+  2:3,
+} */
